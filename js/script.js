6c38020d939c8d80deb89ffa4615664542ffdd0d
Martin-MacDonald <mmacdo54@caledonian.ac.uk>
2017-07-28 19:04:51 +0100

    Animated header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
 
+	if ($(".navToggle").is(":visible")) {
+		$("#mainNav").hide();
+	}
+
+	$(window).on("resize", function(){
+		if ($(window).width() < 1000) {
+		   $("#mainNav").hide();
+		}
+		else {
+		   $("#mainNav").show();
+		}
+	});
+
+	$(".navToggle").click(function(){
+		$("#mainNav").slideToggle();
+	});
 });
