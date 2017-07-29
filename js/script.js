6fc60751dab9418832bd71a0a656544d40ce7321
Martin-MacDonald <mmacdo54@caledonian.ac.uk>
2017-07-29 20:10:03 +0100

    Shell of countdown timer added

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
 $(document).ready(function(){
 
+	var mobNavShown = false;
+
 	if ($(".navToggle").is(":visible")) {
 		$("#mainNav").hide();
+		mobNavShown = false;
 	}
 
 	$(window).on("resize", function(){
-		if ($(window).width() < 1000) {
+		if ($(window).width() < 1000 && mobNavShown == false) {
 		   $("#mainNav").hide();
 		}
-		else {
+		else if ($(window).width() >= 1000){
 		   $("#mainNav").show();
+		   mobNavShown = false;
 		}
 	});
 
 	$(".navToggle").click(function(){
 		$("#mainNav").slideToggle();
+		mobNavShown = !mobNavShown;
 	});
 });
