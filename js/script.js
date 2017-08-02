0cb2c8e4d911eeb663292061aec7bf445071c055
Martin-MacDonald <mmacdo54@caledonian.ac.uk>
2017-08-02 15:39:40 +0100

    Carousel set up initial

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+	//NAVIGATION CONTROL
 	var mobNavShown = false;
 
 	if ($(".navToggle").is(":visible")) {
@@ -21,4 +22,66 @@ $(document).ready(function(){
 		$("#mainNav").slideToggle();
 		mobNavShown = !mobNavShown;
 	});
+
+	//CAROUSEL CONTROL
+	var slideIndex = 1;
+	var numberOfSlides = $(".carouselSlide").length;
+
+	$(".carouselSlide").hide();
+	$(".carouselSlide:nth-of-type(" + slideIndex + ")").show();
+	
+	function changeCircle(index){
+		$(".carouselPicker").removeClass("carouselPickerActive");
+		$(".carouselPicker:nth-of-type(" + index + ")").addClass("carouselPickerActive");
+	}
+
+	changeCircle(slideIndex);
+
+	function changePic(index){
+		$(".carouselSlide").hide();
+		$(".carouselSlide:nth-of-type(" + index + ")").show();
+	}
+
+	function changeIndex(direction){
+		if (direction == "right") {
+			if (slideIndex == numberOfSlides) {
+				slideIndex = 1;
+			} else {
+				slideIndex += 1;
+			}
+			
+		}else{
+			if (slideIndex == 1) {
+				slideIndex = numberOfSlides;
+			} else {
+				slideIndex -= 1;
+			}
+		}
+
+		changePic(slideIndex);
+		changeCircle(slideIndex);
+	}
+
+	$(".slider").on("swiperight",function(){
+		changeIndex("left");
+	});
+
+	$(".slider").on("swipeleft", function(){
+		changeIndex("right");
+	});
+
+	$(".carouselArrow").click(function(){
+		if ($(this).hasClass("fa-angle-right")) {
+			changeIndex("right");			
+		}else{
+			changeIndex("left");			
+		}
+	});
+
+	$(".carouselPicker").click(function(){
+		slideIndex = $(this).index() + 1;
+		changePic(slideIndex);
+		changeCircle(slideIndex);
+	});
+
 });
